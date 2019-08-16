a023586e4bf539b13f4e9976218addaf5c9bb16d
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-08-16 14:07:49 +0200

    Document Paddle

--- Paddle.cc
+++ Paddle.cc
@@ -1,18 +1,34 @@
 #include "Paddle.hh"
 
+
+/**
+ * @brief The Paddle constructor method.
+ *
+ * @param[in]  width   The width of the Paddle
+ * @param[in]  height  The height of the Paddle
+ * @param[in]  speed   The speed of the Paddle
+ */
 Paddle::Paddle (float width, float height, float speed)
 :
+    // Call to super() to initialize the Paddle rectangle
     sf::RectangleShape(sf::Vector2f(width, height)),
+    // Set the Paddle's speed
     _speed(speed)
 {}
 
 
+/**
+ * @brief ++ operator override to move the paddle based on its speed
+ */
 void Paddle::operator++ ()
 {
     setPosition(getPosition().x, getPosition().y + _speed);
 }
 
 
+/**
+ * @brief -- operator override to move the paddle based on its speed
+ */
 void Paddle::operator-- ()
 {
     setPosition(getPosition().x, getPosition().y - _speed);
--- Paddle.hh
+++ Paddle.hh
@@ -4,13 +4,23 @@
 
 using namespace std;
 
+/**
+ * @brief The Paddle class.
+ * 
+ * This class describes the Paddle object, which is what the user controls in
+ * order to deflect the Pong and gain points. It inherits properties from 
+ * SFML's RectangleShape class, as it is basically a rectangle.
+ */
 class Paddle: public sf::RectangleShape {
 
     public:
+        // The constructor method
         Paddle (float width, float height, float speed);
+        // Operator overrides for the ++ and -- operators
         void operator++ ();
         void operator-- ();
 
     private:
+        // The Paddle's speed
         float _speed;
 };
